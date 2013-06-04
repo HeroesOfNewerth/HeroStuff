b960b6cd6dbc7be04ca83aea4a324c17c4be24e2
Vladimir <uavova.k@gmail.com>
2013-06-04 20:53:45 +0300

    Добавление файлов
    Уже немного реализации есть, но работы еще много.

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подобрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Items.AddRange(new object[] {
+            "Amun-Ra",
+            "Ellonia",
+            "War Beast"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 122);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1 игрок",
+            "2 игрока",
+            "3 игрока "});
+            this.comboBox2.Location = new System.Drawing.Point(213, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(228, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.DropDown += new System.EventHandler(this.comboBox2_DropDown);
+            this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2_SelectionChangeCommitted);
+            this.comboBox2.StyleChanged += new System.EventHandler(this.comboBox2_StyleChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Выбор героя:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Выбор героя:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Amun-Ra",
+            "War Beast",
+            "Ellonia"});
+            this.comboBox3.Location = new System.Drawing.Point(12, 190);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 38);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Подобрать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Выбор героя:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Amun-Ra",
+            "War Beast",
+            "Ellonia"});
+            this.comboBox4.Location = new System.Drawing.Point(12, 263);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 11;
+            this.comboBox4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(139, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 52);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Подобрать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(531, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(86, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(531, 121);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(86, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(531, 192);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(86, 65);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(531, 263);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(86, 65);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(383, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(383, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(383, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(383, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 19;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(541, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(66, 32);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Еще >>";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 383);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication4
+{
+
+
+    public partial class Form1 : Form
+    {
+        int herocounter = 0;
+        void Choose2(string[,] mass1, string[,] mass2)
+        {
+            string[,] bothhero = new string[255, 255];
+            int counter = 0, viewcounter = 0;
+            int counter1 = mass1.Length;
+            int counter2 = mass2.Length;
+            for (int i = 0; i < counter1; i++)
+            {
+                for (int f = 0; f < counter2; f++)
+                {
+                    if (mass1[0, i] == mass2[0, f])
+                    {
+                        bothhero[0, counter] = mass1[0, i];
+                        bothhero[1, counter] = mass1[1, i];
+                        counter++;
+                    }
+
+                }
+            }
+            switch (counter)
+            {
+                case 1:
+                    {
+                        label1.Text = bothhero[0, 0];
+                        pictureBox1.Load(bothhero[1, 0]);
+                        if (label4.Text != mass1[0, 1])
+                        {
+                            label5.Text = mass1[0, 1];
+                            pictureBox2.Load(mass1[1, 1]);
+                        }
+                        if (label4.Text != mass1[0, 2])
+                        {
+                            label6.Text = mass2[0, 2];
+                            pictureBox2.Load(mass2[1, 2]);
+                        }
+                        if (label4.Text != mass1[0,3])
+                        {
+                            label7.Text = mass1[0, 3];
+                            pictureBox2.Load(mass1[1, 3]);
+                        }
+
+                        if (label2.Text == "")
+                        {
+                            if (label1.Text != mass1[0, 1] && label3.Text != mass1[0, 1] && label4.Text != mass1[0, 1])
+                            {
+                                label2.Text = mass1[0, 1];
+                                pictureBox2.Load(mass1[1, 1]);
+                            }
+                        }
+                        break;
+
+                    }
+                case 2:
+                    {
+                        label1.Text = bothhero[0, 0];
+                        pictureBox1.Load(bothhero[1, 0]);
+                        label2.Text = bothhero[0, 1];
+                        pictureBox1.Load(bothhero[1, 1]);
+                        if (label1.Text != mass1[0, 1] && label2.Text != mass1[0, 1])
+                        {
+                            label2.Text = mass1[0, 1];
+                            pictureBox2.Load(mass1[1, 1]);
+                        }
+                        break;
+                    }
+            }
+        }
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] amun_ra = new string[,] { { "Ellonia", "Forsaken Archer", "Deadwood", "Sir Benzington","Devourer"},
+            { "http://heroesofnewerth.com/images/heroes/219/icon_128.jpg", "http://heroesofnewerth.com/images/heroes/121/icon_128.jpg", "http://heroesofnewerth.com/images/heroes/123/icon_128.jpg","http://heroesofnewerth.com/images/heroes/234/icon_128.jpg","http://heroesofnewerth.com/images/heroes/6/icon_128.jpg"} };
+        string[] war_beast = new string[] { "Silueth", "SomeAnother", "Empath", "Corrupted Desciple" };
+        string[,] Hero;
+ 
+        void Choose1(string[,] mass1)
+        {
+            if(herocounter < 1) {
+
+            if (mass1.Length == 6 || mass1.Length <6)
+            {
+                label4.Text = mass1[0, 0];
+                pictureBox1.Load(mass1[1, 0]);
+                label5.Text = mass1[0, 1];
+                pictureBox2.Load(mass1[1, 1]);
+                label6.Text = mass1[0, 2];
+                pictureBox3.Load(mass1[1, 2]);
+                herocounter = 3;
+                
+            }
+
+            if (mass1.Length == 8 || mass1.Length > 6)
+            {
+                label4.Text = mass1[0, 0];
+                pictureBox1.Load(mass1[1, 0]);
+                label5.Text = mass1[0, 1];
+                pictureBox2.Load(mass1[1, 1]);
+                label6.Text = mass1[0, 2];
+                pictureBox3.Load(mass1[1, 2]);
+                label7.Text = mass1[0, 3];
+                pictureBox5.Load(mass1[1, 3]);
+                herocounter = 4;
+         
+
+             }
+
+            }
+            else {
+                switch ((mass1.Length/2) - herocounter) 
+                {
+                    case 0:
+                        herocounter = 0; break;
+                    case 1:
+                        {
+                            label5.Text ="";
+                            label6.Text = "";
+                            label7.Text = "";
+                            pictureBox2.Load("empty_legion.jpg");
+                            pictureBox3.Load("empty_legion.jpg");
+                            pictureBox5.Load("empty_legion.jpg");
+                            label4.Text = mass1[0, herocounter];
+                            pictureBox1.Load(mass1[1, herocounter]);
+                            herocounter++;
+                            if (mass1.Length - herocounter == 0) { herocounter = 0; };
+                            break;
+                            
+                        }
+                    case 2:
+                        {
+                            label4.Text = mass1[0, herocounter];
+                            label5.Text = mass1[0, herocounter+1]; 
+                            herocounter +=2;
+                            if (mass1.Length - herocounter == 0) { herocounter = 0; };
+                            break;
+                        }
+                    case 3:
+                        {
+                            label4.Text = mass1[0, herocounter];
+                            label5.Text = mass1[0, herocounter + 1];
+                            label6.Text = mass1[0, herocounter + 2];
+                            herocounter +=3;
+                            if (mass1.Length - herocounter == 0) { herocounter = 0; };
+                            break;
+                        }
+                    case 4:
+                        {
+                            label4.Text = mass1[0, herocounter];
+                            label5.Text = mass1[0, herocounter + 1];
+                            label6.Text = mass1[0, herocounter + 2];
+                            label7.Text = mass1[0, herocounter + 2];
+                            herocounter +=4;
+                            if (mass1.Length - herocounter == 0) { herocounter = 0; };
+                            break;
+                        }
+                
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                       if (comboBox1.Text == "Amun-Ra")
+            {
+                Hero = amun_ra;
+                Choose1(amun_ra);
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem == "1 Игрок")
+            {
+
+                
+                comboBox1.Visible = true;
+                comboBox3.Visible = false;
+                comboBox4.Visible = false;
+                button1.Visible = true;
+                button2.Visible = false;
+                button3.Visible = false;
+            
+            }
+        }
+
+        private void comboBox2_DropDown(object sender, EventArgs e)
+        {
+             
+            
+        }
+
+        private void comboBox2_StyleChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Choose1(Hero);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
